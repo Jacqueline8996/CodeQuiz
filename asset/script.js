f9948dfc7cdeb1010b9a6e116bd9e86eddf4cc64
Jacqueline Rivera <Jacqueline8996@gmail.com>
2020-12-20 14:15:32 -0500

    fixed timer

--- asset/script.js
+++ asset/script.js
@@ -1,9 +1,10 @@
 //getting elements //
+var startEl = document.querySelector("#start-button");
 var highscoreEL = document.querySelector(".highscore");
 var timerEL = document.querySelector(".time");
 var questtionEl = document.querySelector(".question");
 var buttonEl =document.querySelector("#choice");
-var timeLeft = 100;
+var sec = 10;
 var Questions = ["what is the purpose of bootstrap?","What is an array?", "what is the correct syntax to end a comand?",
 "what type of language is Javascript?"];
 //var A1 = [];
@@ -13,28 +14,24 @@ var Questions = ["what is the purpose of bootstrap?","What is an array?", "what 
 
 
 //timer for quiz
-
-//start timer 
-buttonEl.addEventListener("click", scoreKeeper);
-
-function scoreKeeper(){
-    
-    buttonEl = setInterval(function(){
-        timerEl.textContent = "Score:" + timeLeft ;
-        timeLeft--;
-
-        if (timeLeft === 0) {
-        timerEl.textContent = "";
-        clearInterval(buttonEl)
+function startTimer(){
+    //start the counting in miliseconds
+    var timer = setInterval(function(){
+        //add this to HTML
+        timerEL.innerHTML='Time:'+sec;
+        sec--;
+        if (sec < 0) {
+            timerEL.innerHTML='Time:'+ 0
+            clearInterval(timer);
         }
-
+        console.log('i am out');
     }, 1000);
-    
 }
+
 
 function startgame(){
-    scoreKeeper();
+    startTimer();
 
 }
 
-buttonEl.addEventListener("click",startgame);
+startEl.addEventListener("click",startgame);

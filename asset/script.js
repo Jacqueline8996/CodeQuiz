d44d99a5968817313cb661fe8fe5303393a21099
Jacqueline Rivera <Jacqueline8996@gmail.com>
2020-12-20 12:43:38 -0500

    reformating the quiz

--- asset/script.js
+++ asset/script.js
@@ -2,20 +2,39 @@
 var highscoreEL = document.querySelector(".highscore");
 var timerEL = document.querySelector(".time");
 var questtionEl = document.querySelector(".question");
-var startTime = 100
+var buttonEl =document.querySelector("#choice");
+var timeLeft = 100;
+var Questions = ["what is the purpose of bootstrap?","What is an array?", "what is the correct syntax to end a comand?",
+"what type of language is Javascript?"];
+//var A1 = [];
+//var quiz = {
+//    what is the purpose of bootstrap :"",
+//};
 
 
 //timer for quiz
 
 //start timer 
-var timeInterval = setInterval(function() {
-    timerEl.textContent = "Score:" + timeLeft ;
-    timeLeft--;
+buttonEl.addEventListener("click", scoreKeeper);
 
-    if (timeLeft === 0) {
-      timerEl.textContent = "";
-      
-      clearscore(timeInterval);
-    }
+function scoreKeeper(){
+    
+    buttonEl = setInterval(function(){
+        timerEl.textContent = "Score:" + timeLeft ;
+        timeLeft--;
 
-  }, 1000);
+        if (timeLeft === 0) {
+        timerEl.textContent = "";
+        clearInterval(buttonEl)
+        }
+
+    }, 1000);
+    
+}
+
+function startgame(){
+    scoreKeeper();
+
+}
+
+buttonEl.addEventListener("click",startgame);

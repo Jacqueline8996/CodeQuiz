46b26db51fb3bfc3276f2bd8685e06d255926812
Jacqueline Rivera <Jacqueline8996@gmail.com>
2020-12-20 23:38:51 -0500

    added in questions

--- asset/script.js
+++ asset/script.js
@@ -8,15 +8,47 @@ var cardEl =document.querySelector(".card");
 var cardTitleEl =document.querySelector(".card-title");
 var cardOptionsEl =document.querySelector(".card-options");
 var rowScores = document.querySelector(".userInfoRow");
-var goBackEl = document.querySelector(".Go-Back");
+var highscoreTable = document.querySelector("HighscoreTable");
+var highscoreRow = document.querySelector("userInfoRow");
 var sec = 10;
 var zero = 0
-var Questions = ["what is the purpose of bootstrap?","What is an array?", "what is the correct syntax to end a comand?",
-"what type of language is Javascript?"];
-//var A1 = [];
-//var quiz = {
-//    what is the purpose of bootstrap :"",
-//};
+var quizObj = [
+    {
+        "Questions":"what is the purpose of bootstrap?",
+        "Answer1":"template to build website",
+        "Answer2":"fancy boots",
+        "Answer3":"items",
+        "Answer4":"documents"
+    },
+    {
+        "Questions":"What is an array?",
+        "Answer1":"a boquet of flowers",
+        "Answer2":"code",
+        "Answer3":"a list",
+        "Answer4":"a object"
+    },
+    {
+        "Questions":"Which languages do not use semi-colons to end code?",
+        "Answer1":"c++",
+        "Answer2":"python",
+        "Answer3":"javascript",
+        "Answer4":"java"
+    },
+    {
+        "Questions":"What version of HTML are we on?",
+        "Answer1":"HTML4",
+        "Answer2":"HTML5",
+        "Answer3":"HTML7",
+        "Answer4":"HTML6"
+    },
+
+
+];
+var quest = ["what is the purpose of bootstrap?","What is an array?","Which languages do not use semi-colons to end code?","What version of HTML are we on?"];
+var a1 = ["template to build website","fancy boots","items","documents"];
+var a2 = ["a boquet of flowers","a boquet of flowers","code","a list","a object"];
+var a3 =["c++","python","javascript","java"];
+var a4 = ["HTML4","HTML5","HTML7","HTML6"];
 
 
 //timer for quiz
@@ -33,57 +65,39 @@ function startTimer(){
         }
        
     }, 1000);
-}
+};
 
 //function to display highscore
 function highscoreDis(){
     console.log("i am in highscoreDis")
     scr = "highscore.html"
     
-}
+};
 
-function goBack() {
-    window.history.go(-1);
-}
-
-//Startscreen display
-function displayStart(){
-    navbarDisplay();
-    cardTitleEl.textContent ="Code Quiz Challenge";
-    cardOptionsEl.textContent ="Try to answer the following questions within the following time limit Keep in mind that incorrect answer will be penalized your score time by 10 seconds";
-    
-
-}
-
-function navbarDisplay(){
-    timerEL.innerHTML='Time:'+ zero;
-    viewScoreEL.innerHTML="View Highscore";
-
-    if(true){
-        viewScoreEl.addEventListener("click",highscoreDis);
-        highscoreDis();
-    }
-}
 
 function quizStart(){
     var score = startTimer();
-    cardEl.cle
-    for (i = 0; i < Questions.length; i++) {
-        cardTitleEl.textContent = Questions[i]
+    
+    for (i = 0; i < quest.length; i++) {
+        var que = quest[i];
+        console.log(que);
+        cardTitleEl.textContent.que;
+        for (i2 = 0; i2 < a1.length; i2++) {
+            var ans = a1[i2];
+            console.log[ans];
+        };
         
-        
-        i += 1;
-    }
-    return score;
+    };
 
-
-}
+};
+//function displayStart(){
+//};
 
 function startgame(){
     quizStart();
 
-}
+};
 //When star is pressed it start the game 
 startEl.addEventListener("click",startgame);
 
-displayStart();
+//displayStart();
